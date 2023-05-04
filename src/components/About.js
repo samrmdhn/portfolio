3695992658b4f92016a8fb336634511e8c33843f
samrmdhn <thesyams.rebel8@gmail.com>
2023-05-04 11:46:59 +0700

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,4 @@
 import { Box } from "@mui/material";import Text from "./Text";
-
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,13 +8,9 @@ export default function About() {
       <Text variant="primary" style={{ margin: "10px 0px" }}>
         About
       </Text>
+
       <Text variant="primary-3">
-        Interested seeing things in design perspective, especially for web
-        development. Design and designing is something that got myself hyped the
-        most. Through design, someone can make a timeless art on his/her media.
-        Website design is for an example. Keeping up with recent design trend
-        and platforms around the globe. Besides design, i'm an active learner
-        through the internet.
+        Interested seeing things in design perspective,
       </Text>
 
       <Box display="flex" alignItems="center" gap="10px">
